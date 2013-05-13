22fe53a75358f36a1be061ca1406c7d2d562e3ce
repli <repli@repli-ubuntu.(none)>
2013-05-13 14:20:25 +0200

    added source code

---
+++ mailbox.c
@@ -0,0 +1,127 @@
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+#include "sem.h"
+#include "mailbox.h"
+#include "cbuffer.h"
+
+#define UMAX(a, b)      ((a) > (b) ? (a) : (b))
+
+/* El buzón en sí se crea mediante una llamada a mbox_new, que realiza el programa principal.
+ * Como el buzón está en el "heap" es COMPARTIDO POR TODOS LOS HILOS.
+ * TODOS los campos de la siguiente estructura están en memoria compartida.
+ */
+struct sys_mbox {
+	cbuffer_t* cbuffer; /* Buffer circular en el que se almacenan los mensajes */
+	struct sys_sem *not_empty; /* Semáforo para esperar hasta que el buzón no esté vacío */
+	struct sys_sem *not_full; /* Semáforo para esperar hasta que el buzón no esté lleno */
+	pthread_mutex_t *mutex; /* Cerrojo para garantizar exclusión mutua */
+	int wait_send; /* Contador de hilos que están bloqueados en un mbox_post */
+};
+
+/*-----------------------------------------------------------------------------------*/
+
+struct sys_mbox* mbox_new(unsigned int max_size) {
+	struct sys_mbox *mailbox;
+
+	mailbox = (struct sys_mbox *) malloc(sizeof(struct sys_mbox));
+
+	if (mailbox == NULL) {
+		return NULL;
+	}
+
+	/* Create the data structure */
+	if ((mailbox->cbuffer = create_cbuffer_t(max_size)) == NULL) {
+		free(mailbox);
+		return NULL;
+	}
+	mailbox->not_empty = sys_sem_new(0);
+	mailbox->not_full = sys_sem_new(0);
+	mailbox->mutex = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(mailbox->mutex, NULL );
+
+	mailbox->wait_send = 0;
+	return mailbox;
+}
+
+/*-----------------------------------------------------------------------------------*/
+void mbox_free(struct sys_mbox *mbox) {
+	if ((mbox != NULL)) {
+
+		sys_sem_free(mbox->not_empty);
+		sys_sem_free(mbox->not_full);
+
+		pthread_mutex_destroy(mbox->mutex);
+		free(mbox->mutex);
+		destroy_cbuffer_t(mbox->cbuffer);
+
+		mbox->not_empty = mbox->not_full = NULL;
+		mbox->mutex = NULL;
+		mbox->cbuffer = NULL;
+
+		free(mbox);
+
+	}
+}
+
+/** mbox_post (equivalente a un productor)
+ ** Debe escribir el mensaje "msg" en mbox->cbuffer siempre que haya espacio
+ ** Si el buffer está lleno, se incrementerá wait_send (para hacer saber que hay hilos esperando),
+ ** y el hilo se quedará bloqueado hasta que el hilo productor invoque a mbox_fetch().
+ ** Tras salir del bloqueo, se decrementerá wait_send y se volverá a comprobar si hay espacio (bucle).
+ ** Una vez haya espacio, se debe insertar el mensaje en el buffer
+ ** En caso de que el buffer estuviera vacío ANTES de esa escritura (es decir,
+ ** si este post supone introducir el ÚNICO elemento en el mailbox)
+ ** se "informará" a potenciales hilos que estuvieran esperando en un "mbox_fetch()" a que hubiera elementos que consumir del buffer
+ **/
+/*-----------------------------------------------------------------------------------*/
+void mbox_post(struct sys_mbox *mbox, void *msg) {
+
+	pthread_mutex_lock(mbox->mutex);
+
+	while (is_full_cbuffer_t(mbox->cbuffer)) {
+		mbox->wait_send++;
+		pthread_mutex_unlock(mbox->mutex);
+		sys_sem_wait(mbox->not_full);
+		pthread_mutex_lock(mbox->mutex);
+		mbox->wait_send--;
+	}
+
+	if (mbox->cbuffer->size == 0) {
+		sys_sem_signal(mbox->not_empty);
+	}
+
+	insert_cbuffer_t(mbox->cbuffer, msg);
+	pthread_mutex_unlock(mbox->mutex);
+}
+
+/*-----------------------------------------------------------------------------------*/
+/** mbox_fetch --> cliente
+ ** Codificación simétrica a la de post
+ ** El hilo invocador comprobará si hay elementos en el buffer circular. Si es así se quedará bloqueado hasta que haya elementos.
+ ** A continuación extraerá el primer mensaje del buffer. Una vez, hecho esto, deberá comprobarse si
+ ** hay hilos esperando en "post" (es decir, si wait_send es mayor de 0)
+ ** y, en tal caso, despertar a uno de esos hilos.
+ **/
+
+void*
+mbox_fetch(struct sys_mbox *mbox) {
+
+	pthread_mutex_lock(mbox->mutex);
+
+	while (is_empty_cbuffer_t(mbox->cbuffer)) {
+		pthread_mutex_unlock(mbox->mutex);
+		sys_sem_wait(mbox->not_empty);
+		pthread_mutex_lock(mbox->mutex);
+	}
+
+	void* msg = head_cbuffer_t(mbox->cbuffer);
+	remove_cbuffer_t(mbox->cbuffer);
+	sys_sem_signal(mbox->not_full);
+	pthread_mutex_unlock(mbox->mutex);
+	return msg;
+}
+
+/*-----------------------------------------------------------------------------------*/
+
---
+++ sem.c
@@ -0,0 +1,72 @@
+
+#include "sem.h"
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+/*-----------------------------------------------------------------------------------*/
+struct sys_sem * sys_sem_new(int count)
+{
+	struct sys_sem *sem;
+	
+	sem = (struct sys_sem *)malloc(sizeof(struct sys_sem));
+	if (sem != NULL) {
+		sem->c = count;
+		pthread_cond_init(&(sem->cond), NULL);
+		pthread_mutex_init(&(sem->mutex), NULL);
+	}
+	return sem;
+}
+/*-----------------------------------------------------------------------------------*/
+
+
+void sys_sem_free(struct sys_sem *sem)
+{
+	pthread_cond_destroy(&(sem->cond));
+	pthread_mutex_destroy(&(sem->mutex));
+	free(sem);
+}
+
+/*-----------------------------------------------------------------------------------*/
+/** Wait de semaforo binario
+ ** En caso de que el contador (sem->c) sea igual a 0, debe quedarse bloqueado hasta que
+ **  otro hilo haga un sem_signal. En ese momento, todos los hilos bloqueados en 
+ **  el wait "competirán" por seguir su ejecución (y sólo uno puede lograrlo)
+ **  Tras salir del bloqueo (o si nunca entró en él), decrementará el contador
+ ** y finalizará la ejecución
+ **/
+ int
+ sys_sem_wait(struct sys_sem *sem)
+ {	
+ 	int i = 1;
+ 	while (sem->c == 0)
+ 	{
+ 		if (i==1)
+ 		{
+ 			i=0;
+ 		}
+ 	}
+ 	pthread_mutex_lock(&(sem->mutex));
+ 	sem->c--;
+ 	pthread_mutex_unlock(&(sem->mutex));
+ 	return 0;
+ }
+
+/*-----------------------------------------------------------------------------------*/
+/** Signal de nuestro semaforo binario 
+ **/
+ void
+ sys_sem_signal(struct sys_sem *sem)
+ {
+ 	pthread_mutex_lock(&(sem->mutex));
+
+ 	sem->c = 1;
+ 	
+ 	pthread_cond_broadcast(&(sem->cond));
+ 	pthread_mutex_unlock(&(sem->mutex));
+ }
+
+/*-----------------------------------------------------------------------------------*/
+
+
